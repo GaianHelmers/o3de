f7f7f6efdf58a62f94d1a7c28cb4caebc815cb81
GaianHelmers <gaianhelmers@genomestudios.ca>
2026-03-16 14:47:32 -0600

    Revert ""is reference" check to determine differing node variable display graphic"
    This reverts commit 7a180a8802f3c39812dab31fe0da26041079413a.

--- Gems/GraphCanvas/Code/Source/Components/Slots/Data/DataSlotLayoutComponent.cpp
+++ Gems/GraphCanvas/Code/Source/Components/Slots/Data/DataSlotLayoutComponent.cpp
@@ -761,17 +761,7 @@ namespace GraphCanvas
 
             auto slotId = m_owner.GetEntityId();
             NodePropertyDisplay* nodePropertyDisplay = nullptr;
-
-            if (isReference)
-            {
-                // Reference slots show a variable-selection ComboBox
-                GraphModelRequestBus::EventResult(nodePropertyDisplay, sceneId, &GraphModelRequests::CreateDataSlotVariablePropertyDisplay, typeId, nodeId, slotId);
-            }
-            else
-            {
-                // Value slots show the standard inline property editor
-                GraphModelRequestBus::EventResult(nodePropertyDisplay, sceneId, &GraphModelRequests::CreateDataSlotPropertyDisplay, typeId, nodeId, slotId);
-            }
+            GraphModelRequestBus::EventResult(nodePropertyDisplay, sceneId, &GraphModelRequests::CreateDataSlotPropertyDisplay, typeId, nodeId, slotId);
 
             if (nodePropertyDisplay)
             {
